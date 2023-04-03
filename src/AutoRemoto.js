09fd9d24ea532251e7fda8bbe8d9ef36b0b1ed3e
Osber <osberriojalopez@gmail.com>
2023-04-03 18:55:48 -0400

    cuando se ingresa A avanza una posicion desde la posicion inicial por defecto 0,0N

--- src/AutoRemoto.js
+++ src/AutoRemoto.js
@@ -1,7 +1,16 @@
 function Auto(comando)
 {
     let pos_inicial="0,0N";
-    return pos_inicial;
+    let pos_final;
+    if(comando=="")
+    {
+        pos_final=pos_inicial;
+    }
+    if(comando=="A")
+    {
+        pos_final="0,1N"
+    }
+    return pos_final;
 }
 
 export default Auto;
--- src/AutoRemoto.spec.js
+++ src/AutoRemoto.spec.js
@@ -4,4 +4,7 @@ describe("Avanzar una posicion con el comando A desde una posicion inicial por d
     it("Si el piloto no ingresa una cadena vacia se devuelve la posicion inicial sin cabios", () => {
         expect(Auto("")).toEqual("0,0N");
     });
+    it("Cuando se ingrese el comando A se avanzara 1 piscion sobre la posicion inicial por defecto", () => {
+        expect(Auto("A")).toEqual("0,1N");
+    });
 });
