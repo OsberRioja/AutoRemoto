2176363e82de7987260675dff0aef43d53a14e49
chelo7717 <chelo336@gmail.com>
2023-04-10 12:05:25 -0400

    refactorizando funciones

--- src/AutoRemoto.js
+++ src/AutoRemoto.js
@@ -1,3 +1,33 @@
+function GirarDerecha(orientacion)
+{
+    const orientaciones=[4,3,2,1];
+    const indice=orientaciones.indexOf(orientacion);
+    if(indice!=-1)
+    {
+        const siguiente_indice=(indice+1)%4;
+        orientacion=orientaciones[siguiente_indice];
+    }
+    return orientacion;
+}
+
+function GirarIzquierda(orientacion)
+{
+    orientacion=(orientacion%4)+1;
+    return orientacion;
+}
+
+function CrearSuperficie(filas,columnas)
+{
+    const superficie=[];
+    for (let i = 0; i < filas; i++) {
+        superficie[i] = [];
+        for (let j = 0; j < columnas; j++) {
+            superficie[i][j] = 0;
+        }
+    }
+    return superficie;
+}
+
 function Auto(cadena)
 {
     const cardinales=["N","O","S","E"];
@@ -8,15 +38,8 @@ function Auto(cadena)
     let orientacion=1;
     const filas = 5;
     const columnas = 5;
-    const superficie = [];
-
-    //llenamos la matriz de superficie de 0
-    for (let i = 0; i < filas; i++) {
-        superficie[i] = [];
-        for (let j = 0; j < columnas; j++) {
-            superficie[i][j] = 0;
-        }
-    }
+    let superficie=CrearSuperficie(filas,columnas);
+    
     superficie[inicial_x][inicial_y]=1;//posicion inicial por defecto
     let comandos=cadena.split("");
     for(var i=0;i<comandos.length;i++)
@@ -42,22 +65,14 @@ function Auto(cadena)
                 final_x=final_x+1;                     //avanza una posicion el auto
                 superficie[final_x][final_y]=1;//pone en 1 la posicion actual del auto
             }
-            
-        
         }
         if(comandos[i]=="I")
         {
-            orientacion=(orientacion%4)+1;
+            orientacion=GirarIzquierda(orientacion);
         }
         if(comandos[i]=="D")
         {
-            const orientaciones=[4,3,2,1];
-            const indice=orientaciones.indexOf(orientacion);
-            if(indice!=-1)
-            {
-                const siguiente_indice=(indice+1)%4;
-                orientacion=orientaciones[siguiente_indice];
-            }
+            orientacion=GirarDerecha(orientacion);
         }
 
     }
